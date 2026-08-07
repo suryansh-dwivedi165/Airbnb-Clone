10cffb2a1d3b50b32df783161fa06ce375fa7260
= <=>
2026-08-07 17:33:07 +0530

    Intialise

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-if(process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-} 
-
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
@@ -9,7 +5,7 @@ const app = express();
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const path = require("path");
-const port = 3000;
+const port = process.env.PORT || 3000; 
 const ExpressError = require("./utils/ExpressError.js"); 
 const session = require("express-session"); 
 const MongoStore = require('connect-mongo');
@@ -22,9 +18,6 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js"); 
 
-app.listen(port, () => {
-  console.log("Server start on port 3000");
-});
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -32,32 +25,34 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const dbUrl = process.env.ATLASDB_URL;
+
 main()
-  .then(() => {
-    console.log("Connected to db");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .then(() => {
+        console.log("Connected to db");
+    })
+    .catch((err) => {
+        console.log("MongoDB Connection Error:", err);
+    });
 
 async function main() {
-  await mongoose.connect(process.env.ATLASDB_URL);
-}  
+    await mongoose.connect(dbUrl);
+} 
 
 const store = MongoStore.create({
-  store,
-  mongoUrl: dbUrl,
-  crypto: {
-    secret: process.env.SECRET,
-  },
-  touchAfter: 24*3600
-}); 
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600
+});
 
 store.on("error", () => {
   console.log("Erorr in Mongo session store")
 })
 
 const sessionOptions = {
+  store:store,
   secret:process.env.SECRET,
   resave:false,
   saveUninitialized: false,
@@ -84,16 +79,6 @@ app.use((req, res, next) => {
   next();
 }) 
 
-// app.get("/demouser", async (req, res) => {
-//   let fakeUser = new User({
-//     email:"student@gmail.com",
-//     username:"Suryansh Dwived"
-//   }); 
-
-//   let registerUser = await User.register(fakeUser, "Hello World"); 
-//   res.send(registerUser); 
-// }) 
-
 app.use("/listings", listingRouter); 
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/users", userRouter); 
@@ -105,9 +90,8 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   let { status=500, message="Something went wrong"} = err; 
   res.status(status).render("listings/error.ejs", {message}); 
-  // res.status(status).send(message);
 }) 
 
-// app.get("/", (req, res) => {
-//   res.send("It is the root directory ");
-// }); 
+app.listen(port, () => {
+    console.log("Server is listening "); 
+}); 
